0fb5d326dc7d68d3fa0497aa3898e1faef767494
Aaron Ahlberg <ahlbergac@gmail.com>
2020-05-13 18:41:00 -0600

    initial commit

--- App.js
+++ App.js
@@ -5,6 +5,32 @@ import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View,TouchableOpacity,Linking,ActivityIndicator,FlatList,Let} from 'react-native';
 import{Card,Image} from 'react-native-elements';
 import * as rssParser from 'react-native-rss-parser';
+import moment from "moment";
+
+
+
+
+
+
+
+class NewsCard extends Component{
+  componentDidMount(){
+  }
+  render(){
+    return(
+    <TouchableOpacity onPress={() => Linking.openURL(this.props.uri)}>
+    <Card 
+    title= {this.props.title}
+    >
+      <Text>{this.props.date}</Text>
+      </Card>
+      </TouchableOpacity>
+    );
+  }
+    
+}
+
+
 
 
 
@@ -13,48 +39,85 @@ export default class App extends Component{
     super(props);
     this.state ={ isLoading: true}
   }
-  componentDidMount(){
-    return fetch('https://www.cnbc.com/id/10000664/device/rss/rss.html')
-    .then((response) => response.text())
-    .then((responseData) => rssParser.parse(responseData))
-    .then((rss) => {
-      this.setState({
-        isLoading: false,
-        dataSource: rss.items,
+
+  async componentDidMount(){
+
+    const urls = [
+    'https://www.cnbc.com/id/100003114/device/rss/rss.html',
+    'https://www.cnbc.com/id/100727362/device/rss/rss.html',
+    'https://www.cnbc.com/id/10001147/device/rss/rss.html',
+    'https://www.cnbc.com/id/10000664/device/rss/rss.html',
+    'https://www.cnbc.com/id/19854910/device/rss/rss.html',
+    'https://www.cnbc.com/id/20910258/device/rss/rss.html',
+    'http://feeds.marketwatch.com/marketwatch/bulletins',
+    'http://feeds.reuters.com/reuters/businessNews',
+    'http://feeds.reuters.com/reuters/companyNews',
+    'http://feeds.reuters.com/news/wealth',
+    'http://feeds.reuters.com/Reuters/worldNews',
+    'http://feeds.reuters.com/reuters/technologyNews',
+    'https://seekingalpha.com/market_currents.xml',
+    'https://news.yahoo.com/rss/finance',
+     'http://feeds.marketwatch.com/marketwatch/topstories',
+     'http://feeds.marketwatch.com/marketwatch/realtimeheadlines',
+     'http://feeds.reuters.com/reuters/topNews'
+    ];
+    var data= [];
+    // use map() to perform a fetch and handle the response for each url
+  return( await Promise.all(urls.map(url =>
+      fetch(url)
+      .then((response) => response.text())
+      .then((responseData) => rssParser.parse(responseData))
+      .then((rss) => {
+        data=data.concat(rss.items);
         
-      })
-    for (let i of this.state.dataSource){
-      console.log(i.title);
-    }
-   
-    });
+          })
+          .then((result) =>    
+          this.setState({
+            isLoading:false,
+           dataSource:data.sort((a,b) =>{
+            return new Date(b.published)-new Date(a.published)
+          }), 
+         
+         
+           
+          }) 
+          
+          )
+    ))
+  );
+
+
+ 
+     
+    
   }
 
   render() {
     if(this.state.isLoading){
+      
       return(
         <View style={{flex: 1, padding: 20}}>
           <ActivityIndicator/>
         </View>
       )
+    
     }
+
  
     return (
-    
+      
       <View style={styles.container}>
       <FlatList
           data= {this.state.dataSource}
           renderItem={({item}) => 
-          <TouchableOpacity onPress={() => Linking.openURL(item.links[0].url)}>
-                <Card 
-                title= {item.title}
-                image =   {{ uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/CNBC_logo.svg/701px-CNBC_logo.svg.png'}}
-                >
-                  <Text  >{item.description}</Text>
-                  </Card>
-                  </TouchableOpacity>
+            <NewsCard title= {item.title}
+                      
+                      uri={item.links[0].url}
+                      date={moment(item.published).format( "h:mm:ss a MM DD YYYY")}
+                      
+            />
           }
-          keyExtractor={({item}, index) => item}
+          keyExtractor={({item}, index) => item.id}
         />
 
       </View>
